7ec0cce983d1648413625750465a9496dc305536
skyeBreach <skyebenson@hotmail.com>
2026-05-16 03:04:00 +0100

    fix: add root path to health

--- crates/app/src/router.rs
+++ crates/app/src/router.rs
@@ -4,5 +4,5 @@ use breachlet_core::error::AppResult;
 use crate::{handler::health::health, state::AppState};
 
 pub fn build_app_router(app_state: AppState) -> AppResult<Router<AppState>> {
-    Ok(Router::new().route("health", get(health).with_state(app_state)))
+    Ok(Router::new().route("/health", get(health).with_state(app_state)))
 }
