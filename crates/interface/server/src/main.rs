fe7840c7157c71866ff824288dc9c1a4439068aa
skyeBreach <skyebenson@hotmail.com>
2026-05-16 04:11:34 +0100

    feat: setup axum server
    - implement basic axum main
    - move bin/server to crates/interface/server

---
+++ crates/interface/server/src/main.rs
@@ -0,0 +1,39 @@
+use breachlet_app::{router, state::AppState};
+use breachlet_core::{config::BackendConfig, error::AppResult, tracing::configure_tracing};
+
+use anyhow::Context;
+
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> AppResult<()> {
+    // Configure and Initialise tracing
+    configure_tracing();
+    info!("Starting Breachlet Backend Server...");
+
+    // Load config
+    let config = BackendConfig::load()?;
+    info!("Successfully loaded backend config");
+
+    // Initialise the persistent app state
+    let app_state = AppState::init(config.database).await?;
+    info!("App State successfully initialised");
+
+    // Build the app router with a persistent app state
+    let app = router::build_app_router(app_state).await?;
+    info!("Successfully built app router");
+
+    // Create TCP listener for the HTTP server
+    let listener = tokio::net::TcpListener::bind(config.server.address())
+        .await
+        .with_context(|| format!("Failed to bind TCP listener on {}", config.server.address()))?;
+    info!("listening on http://{}", &config.server.address());
+
+    // Serve the app via the TCP listener
+    axum::serve(listener, app)
+        .await
+        .with_context(|| "HTTP Server exited with error")?;
+    info!("HTTP Server exited with success");
+
+    Ok(())
+}
