79c8420aabbe7609e79ae2b57c3c9070acae0d0d
skyeBreach <skyebenson@hotmail.com>
2026-05-16 18:11:25 +0100

    feat: add email value object

---
+++ crates/domain/src/value_object/email.rs
@@ -0,0 +1,59 @@
+use std::fmt::Display;
+
+use axum::Json;
+use breachlet_core::error::{AppError, AppResult};
+use garde::{Report, external::compact_str::ToCompactString, rules::email::parse_email};
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Email(String);
+
+impl Email {
+    pub fn new(value: String) -> Self {
+        Self(value)
+    }
+
+    pub fn parse(value: String) -> AppResult<Self> {
+        if let Err(inner) = parse_email(value.as_str()) {
+            let mut report = Report::new();
+            report.append(
+                garde::Path::new("email"), //
+                garde::Error::new(inner.to_string()),
+            );
+
+            return Err(AppError::Validation(report));
+        }
+
+        Ok(Self(value))
+    }
+
+    pub fn parse_str(value: &str) -> AppResult<Self> {
+        Self::parse(value.to_string())
+    }
+
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl AsRef<str> for Email {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl From<String> for Email {
+    fn from(value: String) -> Self {
+        Self::new(value)
+    }
+}
+
+impl Display for Email {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
